2806c7a3f8ea78d7d662a292ee20d9e1d8929a70
CasparBosch <1004288@hr.nl>
2023-01-25 15:16:00 +0100

    crud files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+// button component
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+// app start
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
       </header>
     </div>
   );
 }
 
+// app export
 export default App;
